1132f7f756f8bb24fc93352f9c817210afc2d79b
Yipartner <1350690718@qq.com>
2017-12-11 11:34:33 +0800

    Form

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\StudentService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public $studentService;
+    public function __construct(StudentService $studentService)
+    {
+        $this->studentService=$studentService;
+    }
+    public function formPost(Request $request)
+    {
+        $rules=[
+            'name' => 'required',
+            'major' => 'required',
+            'stuId' => 'required',
+            'language' => 'required',
+            'mobile' => 'required',
+            'email' => 'required'
+        ];
+        $message=[
+            'required' => 'The :attribute field is required.',
+        ];
+        Validator::make($request->all(),$rules,$message);
+        $date=[];
+        foreach ($rules as $key => $value)
+        {
+            $date[$key]=$request->input($key);
+        }
+        $id=$this->studentService->isStudentExist($date['mobile']);
+        if (!$id) {
+            $this->studentService->studentInsert($date);
+            return response()->json([
+                'code' => 0,
+                'message' => 'insert success'
+            ]);
+        }
+        else {
+            $this->studentService->studentUpdate($id, $date);
+            return response()->json([
+                'code' =>0,
+                'message' => 'update success'
+            ]);
+        }
+
+    }
+}
---
+++ app/Service/StudentService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use Illuminate\Support\Facades\DB;
+
+class StudentService
+{
+
+    public function studentInsert($date)
+    {
+        if (DB::table('students')->insert($date))
+            return true;
+        else
+            return false;
+    }
+
+    public function isStudentExist($studentMobile)
+    {
+        $student=DB::table('students')->where('mobile','=',$studentMobile)->first();
+        if ($student!=null)
+            return $student->id;
+        else
+            return false;
+    }
+    public function studentUpdate($id,$date)
+    {
+        DB::table('students')
+            ->where('id','=',$id)
+            ->update($date);
+    }
+
+
+}
